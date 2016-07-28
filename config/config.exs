74aa9c79a19ca7b001ae7ff6a4bdc88de1d38ddb
ggb <gregorbg@web.de>
2016-07-28 17:06:32 +0200

    almost working phoenix app

---
+++ config/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :mychat, Mychat.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "5IkJ4msbA/4DOBci99bIGv7ksxZ3u6NJng+YwICKpLw8XXRZiS7FUtYA7WYSIwaw",
+  render_errors: [view: Mychat.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Mychat.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ web/channels/user_socket.ex
@@ -0,0 +1,16 @@
+defmodule Mychat.UserSocket do
+  use Phoenix.Socket
+
+  ## Channels
+  channel "room:*", Mychat.RoomChannel
+
+  ## Transports
+  transport :websocket, Phoenix.Transports.WebSocket
+  transport :longpoll, Phoenix.Transports.LongPoll
+
+  def connect(_params, socket) do
+    {:ok, socket}
+  end
+
+  def id(_socket), do: nil
+end
